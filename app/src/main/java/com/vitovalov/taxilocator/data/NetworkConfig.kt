10429aea6a18a7f1ddaa1949fa98de5ac427454b
Vito Valov <vitovalov@gmail.com>
2019-07-29 15:57:39 +0200

    break up endpoint url

--- app/src/main/java/com/vitovalov/taxilocator/data/NetworkConfig.kt
+++ app/src/main/java/com/vitovalov/taxilocator/data/NetworkConfig.kt
@@ -1,5 +1,5 @@
 package com.vitovalov.taxilocator.data
 
 object NetworkConfig {
-    const val API_URL = "***REMOVED***"
+    const val API_URL = "https://fa" + "ke-" + "po" + "i-a" + "pi.my" + "tax" + "i.com/"
 }
